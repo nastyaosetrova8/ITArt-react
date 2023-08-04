5cafc91bf101204638d0c15b81022917e9119d26
GKastet <127600582+GKastet@users.noreply.github.com>
2023-08-04 12:27:56 +0200

    validation

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -1,6 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { logInUserThunk } from 'redux/Thunks/AuthUserThunk';
-
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { Button, Stack, TextField } from '@mui/material';
 
 export const LogInForm = () => {
   const dispatch = useDispatch();
@@ -17,24 +19,92 @@ export const LogInForm = () => {
     evt.currentTarget.reset();
   };
 
+  const validationSchema=Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    password: Yup.string().required('Required'),
+  })
+
   return (
-    <>
-      <h3>LogInForm</h3>
-      <form
-        onSubmit={handlerOnSubmit}
-        style={{
-          width: '500px',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '15px',
-        }}
-      >
-        <label htmlFor="">Email</label>
-        <input type="email" name="email" />
-        <label htmlFor="">Password</label>
-        <input type="password" name="password" />
-        <button type="submit">LOG IN</button>
-      </form>
-    </>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={values => handlerOnSubmit(values)}
+      validationSchema={validationSchema}
+    >
+      {formik => (
+        <Stack sx={{ margin: '75px auto', width: '400px' }}>
+          <Form>
+            <Stack spacing={4}>              
+              <TextField
+                variant='standard'
+                label='Email'
+                name='email'
+                type='email'
+                onChange={formik.handleChange}
+                error={formik.errors.email}
+                helperText={
+                  formik.errors.email && 'Please enter a valid email address'
+                }
+              />
+              <TextField
+              variant='standard'
+              label='Password'
+              name='password'
+              type='password'
+              onChange={formik.handleChange}
+              error={formik.errors.password}
+              helperText={
+                formik.errors.password && 'Please enter your valid password'
+              }
+              />
+              <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              mt={4}
+            >
+              <Button
+                variant="contained"
+                type="submit"
+                size="large"
+                //disabled={!formik.isValid}
+              >
+                Log in
+              </Button>
+              <Button
+                variant="contained"
+                type="submit"
+                size="large"
+                //disabled={!formik.isValid}
+              >
+                Register
+              </Button>
+            </Stack>
+            </Stack>
+          </Form>
+        </Stack>
+      )}
+    </Formik>
+
+    //======================================
+    // <>
+    //   <h3>LogInForm</h3>
+    //   <form
+    //     onSubmit={handlerOnSubmit}
+    //     style={{
+    //       width: '500px',
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       gap: '15px',
+    //     }}
+    //   >
+    //     <label htmlFor="">Email</label>
+    //     <input type="email" name="email" />
+    //     <label htmlFor="">Password</label>
+    //     <input type="password" name="password" />
+    //     <button type="submit">LOG IN</button>
+    //     <button type="submit">REGISTER</button>
+
+    //   </form>
+    // </>
   );
 };
