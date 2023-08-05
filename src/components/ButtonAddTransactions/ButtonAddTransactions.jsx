187012c58e0a929b05db9f1feb04cf46bd80d399
GKastet <127600582+GKastet@users.noreply.github.com>
2023-08-05 19:27:10 +0200

    login style

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,6 +1,13 @@
+import { useSelector } from "react-redux";
+import { selectToken } from "redux/selectors";
+
 export const ButtonAddTransactions = () => {
+  const qqq = useSelector(selectToken)
+  
+
   const handleClick = () => {
     console.log('axaxax');
+    console.log(qqq);
   };
   return (
     <button type="button" onClick={handleClick}>
--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -3,7 +3,16 @@ import { Link } from 'react-router-dom';
 import { logInUserThunk } from 'redux/Thunks/AuthUserThunk';
 import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Button, Stack, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField,
+} from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
+import HttpsIcon from '@mui/icons-material/Https';
 
 export const LogInForm = () => {
   const dispatch = useDispatch();
@@ -28,22 +37,48 @@ export const LogInForm = () => {
       validationSchema={validationSchema}
     >
       {formik => (
-        <Stack sx={{ margin: '75px auto', width: '400px' }}>
+        <Stack sx={{ margin: '75px auto'}}>
           <Form>
             <Stack spacing={4}>
+              {/* <Box
+                sx={{ display: 'flex', alignItems: 'flex-end', width: '100%' }}
+              >
+                <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                <TextField
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  
+                  style={{ width: '100%' }}
+                  variant="standard"
+                  label="Email"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.email)}
+                  helperText={
+                    formik.errors.email && 'Please enter a valid email address'
+                  }
+                />
+              {/* </Box> */}
+              {/* <Box
+                sx={{ display: 'flex', alignItems: 'flex-end', width: '100%' }}
+              >
+                <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+
               <TextField
-                variant="standard"
-                label="Email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                error={Boolean(formik.errors.email)}
-                 helperText={
-                  formik.errors.email &&
-                  'Please enter a valid email address'
-                }
-              />
-              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <HttpsIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                style={{ width: '100%' }}
                 variant="standard"
                 label="Password"
                 name="password"
@@ -54,8 +89,10 @@ export const LogInForm = () => {
                   formik.errors.password && 'Please enter your valid password'
                 }
               />
+              {/* </Box> */}
               <Stack
-                direction="row"
+                direction="column"
+                spacing={4}
                 justifyContent="space-between"
                 alignItems="center"
                 mt={4}
@@ -64,7 +101,7 @@ export const LogInForm = () => {
                   variant="contained"
                   type="submit"
                   size="large"
-                  //disabled={!formik.isValid}
+                  
                 >
                   Log in
                 </Button>
@@ -73,8 +110,7 @@ export const LogInForm = () => {
                   <Button
                     variant="contained"
                     type="submit"
-                    size="large"
-                    //disabled={!formik.isValid}
+                    size="large"                    
                   >
                     Register
                   </Button>
