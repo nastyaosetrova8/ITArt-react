3d53472ad44b6397afd9217cf6b3ddf46d7acebb
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-08-06 20:07:33 +0200

    add

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,6 @@
 export const selectUserName = state => state.register.user.username;
 export const selectBalance = state => state.register.user.balance;
 export const selectToken = state => state.register.token;
-export const selectBalance = state => state.register.user.balance
 
 export const selectIsAuth = state => state.register.isAuth;
 export const selectTransactions = state => state.transactions;
